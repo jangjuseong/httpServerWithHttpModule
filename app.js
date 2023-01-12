a0d8b2bbece2cc7a001438a6870c88754b06b0e1
juseong <jjs7280@gmail.com>
2023-01-12 17:00:46 +0900

    [ADD] 게시물 조회 엔드포인트

--- app.js
+++ app.js
@@ -33,6 +33,33 @@ const server = http.createServer();
 const httpRequestListener = function (request, response) {
   const { url, method } = request;
 
+  if (method === "GET") {
+    if (url === "/lists") {
+      const data = [];
+      for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < posts.length; j++) {
+          if (users[i].id === posts[j].userId) {
+            let userPost = {
+              userID: users[i].id,
+              userName: users[i].name,
+              postingID: posts[j].id,
+              postingTitle: posts[j].title,
+              postingContent: posts[j].content,
+            };
+            data.push(userPost);
+          }
+        }
+      }
+      console.log(data);
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(
+        JSON.stringify({
+          data: data,
+        })
+      );
+    }
+  }
+
   if (method === "POST") {
     if (url === "/users/signup") {
       let body = "";
@@ -54,8 +81,6 @@ const httpRequestListener = function (request, response) {
           JSON.stringify({
             message: "userCreated",
             users: users,
-            // BUG "users": users, 왜 users 따옴표 없어지냐?
-            // FIXED 원래 key 값 부분에는 안되는듯
           })
         );
       });
@@ -95,10 +120,15 @@ server.listen(PORT, IP, function () {
   console.log(`Listening to request on ip ${IP} & port ${PORT}`);
 });
 
-/* NOTE 유저 정보 테스트 입력 값
+/* 
+NOTE 유저 정보 테스트 입력 값
 http -v POST 127.0.0.1:8000/users/signup id:=3 name="김테스트" email="test@gmail.com" password="test_password"
  */
 /* 
 NOTE 게시물 생성 테스트 입력 값
 http -v POST 127.0.0.1:8000/posts id:=3 title="테스트 타이틀" content="테스트 내용" userId:=2            
+ */
+/* 
+NOTE 게시글 목록 테스트 입력 값
+http -v GET 127.0.0.1:8000/lists            
  */
